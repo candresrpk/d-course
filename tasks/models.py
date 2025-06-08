2d1689c81fb6c103ba10b4c545f0b0b60fd00277
Carlos Reyes <andrespk3000@gmail.com>
2025-06-07 22:16:31 -0500

    tasks model created

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.utils.text import slugify
+import uuid
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+    slug = models.SlugField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title + '-' + str(uuid.uuid4()[:4]))
+        super().save(*args, **kwargs)
+        
+        return self
+    
+    def get_absolute_url(self):
+        return f'/tasks/{self.slug}'
+    
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        
